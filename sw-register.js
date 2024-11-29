02ac7b8a3627657542dbcb2395ce2d4c0bae8893
Mkkarwani <147414429+Mkkarwani@users.noreply.github.com>
2024-11-30 00:39:03 +0530

    Update

---
+++ sw-register.js
@@ -0,0 +1,17 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/PWA/sw.js')
+            .then((registration) => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch((error) => {
+                console.error('Service Worker registration failed:', error);
+            });
+    });
+}
+
+// Reload Page When Online
+window.addEventListener('online', () => {
+    console.log('Back online: Reloading to fetch latest content.');
+    location.reload();
+});
